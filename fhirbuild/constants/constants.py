19fee8b06f43d95fb6080df0e2bc9ee32b0e3464
Mario Schattschneider <mario.schattschneider@med.uni-greifswald.de>
2026-03-30 12:35:33 +0200

    Start with constants for codesystems

---
+++ fhirbuild/constants/constants.py
@@ -0,0 +1,12 @@
+
+IDCONTAINERTYPE = "idcontainertype"
+
+
+CODESYSTEMS = {
+    "CXX3": {
+        IDCONTAINERTYPE: "urn:centraxx"
+    },
+    "CXX4": {
+        IDCONTAINERTYPE: "https://fhir.centraxx.de/system/idContainerType"
+    }
+}
